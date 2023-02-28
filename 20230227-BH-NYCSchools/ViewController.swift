d3e48e6d41897f3f3f5af710860122cfc8937f29
Brandon <huabrandon0@gmail.com>
2023-02-27 21:48:13 -0800

    Initial Commit

---
+++ 20230227-BH-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230227-BH-NYCSchools
+//
+//  Created by Brandon Hua on 2/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
